bdbf6a38145e9f1dd70eb4a1b7f10d6f37c06e86
Esraa Abdel-Raheem <abdelraheemesraa@gmail.com>
2020-11-02 19:45:06 +0200

    adjust the styling to be the same as the desgin

--- src/components/RssChannel.vue
+++ src/components/RssChannel.vue
@@ -4,19 +4,22 @@
       <b> {{ readerData.feed.title }} </b>
     </p>
     <sequential-entrance tag="div" fromTop>
-      <div
-        v-for="(item, index) in readerData.items.slice(0, 9)"
-        :key="index"
-        class="one-news col-md-4 d-inline-block p-1">
-        <section class="news-body p-3">
-          <a :href="item.link" class="news-title">
-            <b>
-              {{ item.title }}
-            </b>
-          </a>
-          <p class="news-date">{{ item.pubDate }}</p>
-        </section>
-        <img class="news-image" :src="item.enclosure.link" />
+      <div class="news-container">
+        <div
+          v-for="(item, index) in readerData.items.slice(0, 7)"
+          :key="index"
+          class="one-news p-1"
+        >
+          <section class="news-body p-2">
+            <a :href="item.link" class="news-title">
+              <b>
+                {{ item.title }}
+              </b>
+            </a>
+            <p class="news-date">{{ item.pubDate }}</p>
+          </section>
+          <img class="news-image" :src="item.enclosure.link" />
+        </div>
       </div>
     </sequential-entrance>
   </div>
@@ -46,30 +49,50 @@ export default {
 @white: #fff;
 .reader-container {
   margin: 20px 0;
-  .one-news {
-    position: relative;
-    box-shadow: 0px 0px 5px rgba(129, 129, 212, 0.8);
-    .news-body {
-      display: flex;
-      flex-direction: column;
-      position: absolute;
-      box-sizing: border-box;
-      color: @white;
-      cursor: pointer;
-      .news-title {
+  .news-container {
+    display: inline-grid;
+    grid-template-columns: 35% 35% 35%;
+    grid-column-gap: 10px;
+    grid-row-gap: 10px;
+    .one-news:first-child {
+      grid-column-start: 1;
+      grid-column-end: 3;
+      grid-row-start: 1;
+      grid-row-end: 2;
+    }
+    .one-news:last-child {
+      grid-column-start: 2;
+      grid-column-end: 4;
+      grid-row-start: 3;
+      grid-row-end: 3;
+    }
+    @media (min-device-width: 280px) and (max-device-width: 700px) {
+      grid-template-columns: 100%;
+    }
+    .news-image {
+      width: 100%;
+      height: 100%;
+    }
+    .one-news {
+      position: relative;
+      box-shadow: 0px 0px 5px rgba(129, 129, 212, 0.8);
+      .news-body {
+        display: flex;
+        flex-direction: column;
+        position: absolute;
+        box-sizing: border-box;
         color: @white;
-        @media (min-device-width: 768px) and (max-device-width: 1024px) {
-          font-size: small;
+        .news-title {
+          color: @white;
+          @media (min-device-width: 768px) and (max-device-width: 1024px) {
+            font-size: small;
+          }
+        }
+        .news-date {
+          top: 70px;
         }
       }
-      .news-date {
-        top: 70px;
-      }
     }
-  }
-  .news-image {
-    width: 100%;
-    height: auto;
   }
 }
 </style>
